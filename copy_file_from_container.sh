0ffb1635c3ae58efdb6b557d3c5fdcdf1675ae29
15060868 <15060868@cnsuning.com>
2017-12-21 10:54:22 +0800

    Add action that clear all logs in the /var/log/message.

--- copy_file_from_container.sh
+++ copy_file_from_container.sh
@@ -46,6 +46,13 @@ do
         echo -e "Error: ssh and executed commands failtrue.\n"
     fi
     
+    # clear all logs
+    echo -e "Clear all logs on the $host.\n"
+    ssh root@$host 'echo "" > /var/log/message'
+    if [ $? -ne 0 ];then
+        echo -e "Error: ssh and executed commands failtrue.\n"
+    fi
+    
     # copy files to target host
     for file_item in ${dockers[@]};
     do
